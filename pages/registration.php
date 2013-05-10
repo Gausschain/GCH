9301ab3be9efc3ead79239ef23ca0d04c74fbaf8
Kevin Akos Rankine <kevinranks@gmail.com>
2013-05-09 22:33:52 -0700

    closed registration for now

--- pages/registration.php
+++ pages/registration.php
@@ -10,12 +10,13 @@
 			<h1>Registration</h1>
 		</header>
 		<section>
-			<form name="register" action="register.php" method="post" accept-charset="utf-8">
+			<!--<form name="register" action="register.php" method="post" accept-charset="utf-8">
 				<p>Your email<input type="email" class="emailfield" name="email"></p>
 				<p>Password<input type="password" class="passwordfield" name="password"></p>
 				<p>Password (type again)<input type="password" name="rpassword"></p>
 				<input type='submit' name='Submit' value='Create account'>
-			</form>
+			</form> -->
+			Registration is closed at the moment.
 		</section>
 	</body>
 </html>
