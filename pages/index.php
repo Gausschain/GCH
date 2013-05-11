488201095cf63e69679492967602ac299dc78522
Kevin Akos Rankine <kevinranks@gmail.com>
2013-05-10 21:52:40 -0700

    we good?

--- pages/index.php
+++ pages/index.php
@@ -10,7 +10,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-        <title>Gauss Chains</title>
+        <title>Gauss Chain</title>
         <link rel="stylesheet" href="../styles/default.css">
         <style> 	
         	img {
