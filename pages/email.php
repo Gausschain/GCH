4724eed6bdcecc6fff0829f041622b53bdcea652
Kevin Akos Rankine <kevinranks@gmail.com>
2013-05-11 12:16:04 -0700

    joog

--- pages/email.php
+++ pages/email.php
@@ -1,6 +1,6 @@
 <?php
 require '../sendgrid-php/SendGrid_loader.php';
-require '../Swift-5.0.0/lib/swift_required.php';
+require '../swiftmailer-master/lib/swift_required.php';
 function sendmaile($to_address,$subject,$contents) {
 	$sendgrid=new SendGrid('app15565391@heroku.com','9xx9j1d5');
     	$mail=new SendGrid\Mail();
@@ -10,7 +10,8 @@ function sendmaile($to_address,$subject,$contents) {
   			setSubject('GaussChains')->
   			setText('hey gc man')->
   			setHtml('');
-  	$sendgrid->smtp->send($mail);
+  	mail("kevinranks@gmail.com", "A subject", "A message", "FROM: noreply@mail.com");
+  	//$sendgrid->smtp->send($mail);
 }
 sendmaile('kevinranks@gmail.com','hello','from gausschains');
 ?>
