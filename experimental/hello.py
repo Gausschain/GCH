cd4ec6d892d1cb76226d9b5735077314f1e7342d
Kevin Akos Rankine <kevinranks@gmail.com>
2013-05-21 21:31:15 -0700

    js functionality update

--- experimental/hello.py
+++ experimental/hello.py
@@ -1,1 +1,4 @@
+#!/usr/bin/python2.6
+
+# enable debugging
 print "Hello!"
--- pages/problems/problem.php
+++ pages/problems/problem.php
@@ -14,15 +14,18 @@
       <link rel='stylesheet' type ='text/css' href='../../styles/light.css' title='light'>
     <?php } ?>
     <script language='javascript' type="text/x-mathjax-config">
-        MathJax.Hub.Config({
-        tex2jax: {
-          inlineMath: [['$','$'], ['\\(','\\)']],
-          processEscapes: true
-        }
-      });
+    math();
+      function math() {
+          MathJax.Hub.Config({
+          tex2jax: {
+            inlineMath: [['$','$'], ['\\(','\\)']],
+            processEscapes: true
+          }
+        });
+      }
     </script>
     <script type="text/javascript"
-        src="http://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML">
+        src="http://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML" defer="defer">
     </script> 
 </head>
 <body>
